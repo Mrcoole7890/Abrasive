1f65007f8ed7dc7d618abee23ee48895c13aa7ea
Cole A. Kendall <kendall.cole@revature.com>
2022-01-07 13:33:09 -0500

    add angular front end tracer code

---
+++ Abrasive Front/src/app/componets/login/login.component.ts
@@ -0,0 +1,42 @@
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { Socket } from 'ngx-socket-io';
+import { Observable, Subscription } from 'rxjs';
+
+import { PlayerService } from 'src/app/services/player.service';
+import { Player } from 'src/app/models/player';
+import { LoginService } from 'src/app/services/login.service';
+
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+
+export class LoginComponent implements OnInit {
+  username: String = "";
+  height: String = "";
+  HEIGHT_STYLE_PREPEND: String = "height: ";
+  HEIGHT_STYLE_APPEND: String = "px;";
+  //private _userSub: Subscription;
+
+  constructor(private userService: PlayerService, private socket: Socket, private loginService: LoginService) { 
+    this.height = this.HEIGHT_STYLE_PREPEND + window.innerHeight.toString() + this.HEIGHT_STYLE_APPEND;
+  }
+
+  ngOnInit(): void {
+    this.socket.on("Player Exists", function(user: Player){
+      console.log(user.username + " is already queued up!");
+    });
+    this.socket.on("emptyString", this.userAttemptedToSubmitAEmptyString);
+  }
+
+  newPlayer(): void {
+    this.userService.newPlayer(this.username);
+    this.username = "";
+  }
+
+  userAttemptedToSubmitAEmptyString(): void{
+    console.log("Empty Strings are not acceptible");
+  }
+}
---
+++ Abrasive Front/src/app/models/player.ts
@@ -0,0 +1,8 @@
+export class Player {
+
+    constructor(username: String) {
+        this.username = username;
+    }
+
+    username: String;
+}
